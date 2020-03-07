c125a433a82a5122b80c3234fca78d4e5dda1e78
Simran Jackson <simran.jackson@johnlewis.co.uk>
2020-03-07 16:41:40 +0000

    alter k values calculation for r

--- src/attempt_1.py
+++ src/attempt_1.py
@@ -55,7 +55,8 @@ import matplotlib.pyplot as plt
 
 #%%
 ## constants
-M = 2 * 10 ** 30 ##aolar mass
+M = 2 * 10 ** 30 ##solar mass
+m = 6 * 10 ** 24 ## earth mass
 R = 1.496 * 10 ** 8 ## astronomical unit
 G = 6.67 * 10 ** (-11)
 V = 3 * 10**4 ## earth avg orbital speed
@@ -65,37 +66,47 @@ r0 = np.array([R, 0, 0])
 v0 = np.array([0, V, 0])
 
 
-def runkut(y0, t0, dt, derivs):
+def runkut(r0, v0, t0, dt, v_derivs):
     """
     y0 is value of y at start of timestep
     derivs must be funct of y,t 
     """
     ## runge kutta constants:
-    k1 = derivs(y0, t0)
-    k2 = derivs(y0 + (k1 * (dt/2)), t + (dt/2))
-    k3 = derivs(y0 + (k2 * (dt/2), t + (dt/2)))
-    k4 = derivs(y0 + (k3 * dt), t + dt)
+    vk1 = derivs(v0, t0)
+    rk1 = v0
+    
+    vk2 = derivs(v0 + (vk1 * (dt/2)), t0 + (dt/2))
+    rk2 = v0 + (rk1*(dt/2))
+    #rk2 = drdt(r0 + (rk1 * (dt/2)), t0 + (dt/2))
+    
+    vk3 = derivs(v0 + (vk2 * (dt/2), t0 + (dt/2)))
+    ## rk2 = drdt(r0 + (rk2 * (dt/2)), t0 + (dt/2))
+    rk2 = v0 + 
+    
+    vk4 = derivs(v0 + (vk3 * dt), t0 + dt)
     
     ## y(t+dt)
-    return(y0 + (1/6)*(k1 + 2*k2 + 2*k3 + k4) * dt)
+    return(r0 + (1/6)*(vk1 + 2*vk2 + 2*vk3 + vk4) * dt)
     
-def v_derivs(r, t):
+def dvdt(r, t):
     """
     dv/dt only depends on position, not time
     """    
     r_hat = r/np.linalg.norm(r)
     
-    dvdt = -(G*M/r**2) * r_hat
+    dvdt_vec = -(G*M/r**2) * r_hat
     
-    return(np.nan_to_num(dvdt))             
+    return(np.nan_to_num(dvdt_vec))             
     
  
+#%% 
     
-    
-#def r_derivs(y, t):
-    
-    
-    
+def drdt(r, t):
+    """
+    dr/dt is just v
+    """
+
+#%% 
     
     
     
