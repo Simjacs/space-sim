f94e0047d28bb2f37c1d68d326ff28cd3ce09f1d
Simran Jackson <simran.l.jackson@gmail.com>
2020-09-11 14:26:42 +0100

    transform input and output position data to get correct forms for calculations

--- src/attempt_1.py
+++ src/attempt_1.py
@@ -75,6 +75,7 @@ def sum_acceleration(distances, masses):
     """
     if len(distances) != len(masses):
         print("number of coordinates does not match number of masses")
+        print(distances, masses, len(distances), len(masses))
         raise ValueError
 
     n = len(distances)
@@ -90,51 +91,28 @@ def sum_acceleration(distances, masses):
     return sum(dvdt_array)
 
 
-def transform_values(position_array, velocity_array, mass_index):
+def transform_values(position_array, velocity_array, mass_array, mass_index):
     """
     change positions to distances from test mass
     and remove values for test mass from position and velocity arrays
     :param position_array: array of vector positions for each mass in the system
     :param velocity_array: array of vector velocities for each mass in the system
     :param mass_index: index of mass currently being considered as test mass
-    :return: vector distances from test mass, vector velocities of masses other than test mass
+    :return: vector distances from test mass, vector velocity of test mass, array of values of interacting masses
     """
     mass_position = position_array[mass_index]
     distances = np.delete(position_array, mass_index, 0)
     distances = mass_position - distances
-    velocities = np.delete(velocity_array, mass_index, 0)
 
-    return distances, velocities
+    velocities = velocity_array[mass_index]
+
+    masses = np.delete(mass_array, mass_index, 0)
+
+    return distances, velocities, masses
 
 
 ## advance one timestep:
 def runkut(rt0, vt0, dt, m):
-    """
-    implement 4th order runge-kutta for position and velocity
-    :param rt0: position vector at start of timestep
-    :param vt0: velocity vector at start of timestep
-    :param dt: length of timestep
-    :return: position and velocity vector at t = t0 + dt
-    """
-    vk1 = sum_acceleration(rt0, m)
-    rk1 = vt0
-
-    vk2 = sum_acceleration(rt0 + (rk1 * (dt / 2)), m)
-    rk2 = vt0 + vk1 * (dt / 2)
-
-    vk3 = sum_acceleration(rt0 + (rk2 * (dt / 2)), m)
-    rk3 = vt0 + vk2 * (dt / 2)
-
-    vk4 = sum_acceleration(rt0 + (rk3 * dt), m)
-    rk4 = vt0 + vk3 * dt
-
-    vdt = vt0 + (1 / 6) * (vk1 + 2 * vk2 + 2 * vk3 + vk4) * dt  # velocity at time t0 + dt
-    rdt = rt0 + (1 / 6) * (rk1 + 2 * rk2 + 2 * rk3 + rk4) * dt  # position at time t0 + dt
-
-    return rdt, vdt
-
-
-def vector_runkut(rt0, vt0, dt, m):
     """
     implement 4th order runge-kutta for position and velocity
     :param rt0: position vector at start of timestep
@@ -159,6 +137,26 @@ def vector_runkut(rt0, vt0, dt, m):
 
     return rdt, vdt
 
+
+def vector_runkut(r_0, v_0, delta_t, m):
+    vk1 = sum_acceleration(r_0, m)
+    rk1 = v_0
+
+    vk2 = sum_acceleration(r_0 + (rk1 * (delta_t / 2)), m)
+    rk2 = v_0 + vk1 * (delta_t / 2)
+
+    vk3 = sum_acceleration(r_0 + (rk2 * (delta_t / 2)), m)
+    rk3 = v_0 + vk2 * (delta_t / 2)
+
+    vk4 = sum_acceleration(r_0 + (rk3 * delta_t), m)
+    rk4 = v_0 + vk3 * delta_t
+
+    v_dt = v_0 + (1 / 6) * (vk1 + 2 * vk2 + 2 * vk3 + vk4) * delta_t  # velocity at time t0 + dt
+    r_dt = r_0 + (1 / 6) * (rk1 + 2 * rk2 + 2 * rk3 + rk4) * delta_t  # position at time t0 + dt
+
+    return r_dt, v_dt
+
+
 # %%
 ## constants
 M0 = 2 * 10 ** 30  ##solar mass
@@ -171,24 +169,24 @@ V_peri = 3.029 * 10 ** 4
 R_aphe = 1.01671 * R
 V_aphe = 2.929 * 10 ** 4
 
-#mass_names = ["sun", "sun2", "earth"]
-#masses = [M0, 2*M0, m_e]
+# mass_names = ["sun", "sun2", "earth"]
+# masses = [M0, 2*M0, m_e]
 mass_names = ["sun", "earth"]
-masses = [M0, m_e]
-n = len(masses)
+mass_values = [M0, m_e]
+n = len(mass_values)
 
 ## starting condtions:
-#r0 = np.array([[-1*R, 0, 0], [1*R, 0, 0], [0, 5*R, 0]])
-#v0 = np.array([[0, -10*V, 0], [0, 10*V, 0], [-30*V, 0, 0]])
+# r0 = np.array([[-1*R, 0, 0], [1*R, 0, 0], [0, 5*R, 0]])
+# v0 = np.array([[0, -10*V, 0], [0, 10*V, 0], [-30*V, 0, 0]])
 r0 = np.array([[0, 0, 0], [R, 0, 0]])
-v0 = np.array([[0, 0, 0], [0, 32*V, 0]])
+v0 = np.array([[0, 0, 0], [0, 32 * V, 0]])
 r0_peri = np.array([[0, 0, 0], [R_peri, 0, 0]])
 v0_peri = np.array([[0, 0, 0], [0, V_peri, 0]])
 
 dt = 0.0001
 rt0 = copy.deepcopy(r0_peri)
 vt0 = copy.deepcopy(v0_peri)
-timesteps = 400
+timesteps = 500
 
 rdt = np.zeros((n, 3))
 vdt = np.zeros((n, 3))
@@ -196,38 +194,42 @@ vdt = np.zeros((n, 3))
 r_data = np.zeros([timesteps + 1, n, 3])
 r_data[0] = r0
 
+print(rt0, vt0)
 for i in range(timesteps):  # number of timesteps
     ## at each timestep, calculate the total force acting upon each body
     r_interactions = np.zeros((n, 3))
     v_interactions = np.zeros((n, 3))
-    for m_i in range(n):    # n is number of masses
-        mass = masses[m_i]
-        dists, vels = transform_values(position_array=rt0, velocity_array=vt0, mass_index=m_i)
-        rdt, vdt = runkut(dists, vels, dt, mass)
+    for m_i in range(n):  # n is number of masses
+        mass = mass_values[m_i]
+        dists, vels, masses = transform_values(
+            position_array=rt0, velocity_array=vt0,
+            mass_array=mass_values, mass_index=m_i)
 
-    rt0 = rdt
-    vt0 = vdt
+        ## dil mein mere hai
+        delta_distances, vdt = vector_runkut(r_0=dists, v_0=vels, delta_t=dt, m=masses)
+        r_interactions[m_i] = rt0[m_i] + (dists - delta_distances)
+        v_interactions[m_i] = vdt
 
+    rt0 = r_interactions
+    vt0 = v_interactions
 
-
-
-
+    if i % 10 == 0:
+        print(i)
+        print(rt0, vt0)
 
     r_data[i + 1] = rt0
-print(r_data)
 
 m1_x, m1_y = [r_data[i][0, 0] for i in range(timesteps)], [r_data[i][0, 1] for i in range(timesteps)]
 m2_x, m2_y = [r_data[i][1, 0] for i in range(timesteps)], [r_data[i][1, 1] for i in range(timesteps)]
-#m3_x, m3_y = [r_data[i][2, 0] for i in range(timesteps)], [r_data[i][2, 1] for i in range(timesteps)]
+# m3_x, m3_y = [r_data[i][2, 0] for i in range(timesteps)], [r_data[i][2, 1] for i in range(timesteps)]
 
 fig = plt.figure()
 plt.plot(m1_x, m1_y, markersize=10)
-fig.savefig(f"orbit_dt_{dt}_steps_{timesteps}_mass1.png")
-#plt.plot(m3_x, m3_y)
-fig = plt.figure()
+#fig.savefig(f"orbit_dt_{dt}_steps_{timesteps}_mass1.png")
+#fig = plt.figure()
 plt.plot(m2_x, m2_y, markersize=2)
-fig.savefig(f"obrit_dt_{dt}_steps_{timesteps}_mass2.png")
-
+#fig.savefig(f"obrit_dt_{dt}_steps_{timesteps}_mass2.png")
+fig.savefig(f"orbit_dt_{dt}_steps_{timesteps}.png")
 
 """
 dt = 1
